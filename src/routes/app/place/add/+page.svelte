7b97b4cb605ab3e2d116be053921f633965e90fb
Dan <dan.krajnak@gmail.com>
2023-10-29 15:28:13 -0400

    Use google library loader

--- src/routes/app/place/add/+page.svelte
+++ src/routes/app/place/add/+page.svelte
@@ -3,15 +3,19 @@
   import Place from "$lib/components/Place.svelte";
   import { PlaceResultToLunchPlace } from "$lib/types/LunchPlace";
   import { PUBLIC_GOOGLE_API_ID } from "$env/static/public";
-
-  // export let data: PageData;
+  import { Loader } from "@googlemaps/js-api-loader";
 
   // Add autocomplete
   let autoCompleteInput: HTMLInputElement;
 
   let place: google.maps.places.PlaceResult;
   onMount(() => {
-    const placesApiPromise = google.maps.importLibrary("places");
+    const loader = new Loader({
+      apiKey: PUBLIC_GOOGLE_API_ID,
+      version: "weekly",
+    });
+
+    const placesApiPromise = loader.importLibrary("places");
     let autoCompleteListener: google.maps.MapsEventListener;
     placesApiPromise.then((resp) => {
       const { Autocomplete } = resp as google.maps.PlacesLibrary;
@@ -43,19 +47,6 @@
     };
   });
 </script>
-
-<svelte:head>
-  <!-- this is just stuff from https://developers.google.com/maps/documentation/javascript/places#find_place_requests -->
-  {@html `
-  <!-- prettier-ignore -->
-  <script>
-    (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=\`https://maps.\${c}apis.com/maps/api/js?\`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-      key: "${PUBLIC_GOOGLE_API_ID}",
-      v: "weekly",
-    });
-  </script>
-`}
-</svelte:head>
 
 <div class="flex flex-col my-10 min-h-full item-center">
   <div class="container space-y-4 w-1/2">
